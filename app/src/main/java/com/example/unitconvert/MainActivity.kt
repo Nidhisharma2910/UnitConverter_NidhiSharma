e173d884ec2dfe56fbf5195f6077d45a16aa193a
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-10-26 19:59:04 +0530

    adding unit convertor for area,length, mass

---
+++ app/src/main/java/com/example/unitconvert/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.unitconvert
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.unitconvert.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var languageManager: LanguageManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.title = "Unit Converter"
+        binding.toolbar.subtitle = "Home"
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        languageManager = LanguageManager(this)
+
+        binding.areaCardView.setOnClickListener {
+            val intent = Intent(this, AreaActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.lengthCardView.setOnClickListener {
+            startActivity(Intent(this, LengthActivity::class.java))
+        }
+
+        binding.frequencyCardView.setOnClickListener {
+            startActivity(Intent(this, FrequencyActivity::class.java))
+        }
+        binding.massCardView.setOnClickListener {
+            startActivity(Intent(this, MassActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.setting) {
+            startActivity(Intent(this@MainActivity, SettingActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
